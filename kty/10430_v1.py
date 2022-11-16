a2be4f815c4a36bce56f7fc9b92483d3efbfcb8c
solarkim <ty7613@naver.com>
2022-11-17 02:02:48 +0900

    22.11.17 code study

---
+++ kty/10430_v1.py
@@ -0,0 +1,12 @@
+import numpy
+
+def f1(a,b,c):
+    ans1 = (a+b)%c
+    ans2 = ((a%c)+(b%c))%c
+    ans3 = (a*b)%c
+    ans4 = ((a%c)*(b%c))%c
+    print("{}\n{}\n{}\n{}\n".format(ans1,ans2,ans3,ans4))
+
+if __name__ == '__main__':
+    A, B, C = list(map(int,input().split(" ")))
+    f1(A, B, C)
---
+++ kty/10430_v2.py
@@ -0,0 +1,7 @@
+a, b, c=list(map(int,input().split(" ")))
+
+ans1 = (a+b)%c
+ans2 = ((a%c)+(b%c))%c
+ans3 = (a*b)%c
+ans4 = ((a%c)*(b%c))%c
+print("{}\n{}\n{}\n{}\n".format(ans1,ans2,ans3,ans4))
